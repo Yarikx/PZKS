ba069514b7e8da4125e12026961471f631d8837c
Yarix <Yarikx@gmail.com>
2012-10-21 21:47:10 +0300

    building simple tree works

--- src/org/kpi/pzks/TreeBuilder.scala
+++ src/org/kpi/pzks/TreeBuilder.scala
@@ -9,17 +9,11 @@ class Element;
 case class Op(c: Char) extends Element;
 case class Const(v: Double) extends Element;
 case class Var(v: String) extends Element;
-case class Ob() extends Element
-case class Cb() extends Element
+case class Ob extends Element
+case class Cb extends Element
 case class Expr(elements: List[Element]) extends Element
 
 object TreeBuilder extends App {
-
-  val s = "abc+bcd-5*((abdc*(546.34-3)+3)/2)"
-
-  //  println(elements)
-
-  //  println(zipped)
 
   def groupElements(nodes: Seq[Node], str: String) = {
 
@@ -73,13 +67,49 @@ object TreeBuilder extends App {
 
   }
   
-//  def buildSimpleTree(src: List[Element])={
-//    
-//  }
+  def buildSimpleTree(src: List[Element]): List[Element]={
+    
+    def getElementsAfterBraces(tail: List[Element])={
 
-  val nodes = parseString(s)
+      def recur(l: List[Element], acum: Int):List[Element]={
+        val head :: tail = l
+        head match{
+          case Ob() => recur(tail, acum+1)
+          case Cb() => if(acum == 1) tail else recur(tail,acum-1)
+          case _ => recur(tail,acum)
+        }
+      }
+      
+      recur(tail,1)
+      
+    }
+    
+    val braces = src.zip(0 until src.size).filter(x => x.isInstanceOf[OpenBrace] || x.isInstanceOf[CloseBrace])
+    
+    val (beforeBraces,startOfBraces) = src.span(_ != Ob())
+    if(beforeBraces == src){
+      return beforeBraces
+    }
+    
+    val afterBraces = getElementsAfterBraces(startOfBraces.tail)
+    
+    val treeInBraces = Expr(buildSimpleTree(startOfBraces.tail.take(startOfBraces.size -2 - afterBraces.size)))
+    
+    beforeBraces ::: treeInBraces :: buildSimpleTree(afterBraces)
+    
+  }
   
   
-  println(groupElements(nodes,s))
+  val s = "abc+(123-4/abc+(2-1))"
+  val parsed = parseString(s)
+  val elements = groupElements(parsed, s)
+  
+  println(s)
+  println(buildSimpleTree(elements))
+
+//  val nodes = parseString(s)
+  
+  
+//  println(groupElements(nodes,s))
 
 }
