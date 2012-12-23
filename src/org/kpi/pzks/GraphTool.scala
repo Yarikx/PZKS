9d45bd53915f31637b93f0399d9d012b94613465
Yarix <Yarikx@gmail.com>
2012-12-23 21:02:23 +0200

    remove print

--- src/org/kpi/pzks/GraphTool.scala
+++ src/org/kpi/pzks/GraphTool.scala
@@ -13,23 +13,23 @@ object GraphTool {
 
   val rnd = new Random;
 
-  class El(val id: Int){
+  class El(val id: Int) {
     def leafs = getLeafs(this)
-    def replace(leaf:Oper) = replaceProgress(this, leaf)
-    def rm(leaf:Oper, p:Progress) = removeLeaf(this, leaf, p)
+    def replace(leaf: Oper) = replaceProgress(this, leaf)
+    def rm(leaf: Oper, p: Progress) = removeLeaf(this, leaf, p)
   }
-  case class Oper(c: Char, l: El, r: El) extends El(rnd.nextInt(1000000)){
-    def value={
-      c match{
+  case class Oper(c: Char, l: El, r: El) extends El(rnd.nextInt(1000000)) {
+    def value = {
+      c match {
         case '+' => 1
         case '-' => 1
-        case '*' =>	3
+        case '*' => 3
         case '/' => 5
       }
     }
   }
   case class Val(s: String) extends El(rnd.nextInt(1000000))
-  
+
   def pair(list: List[Element]): List[Element] = {
     if (Expr(list).isPure) {
       list
@@ -73,12 +73,12 @@ object GraphTool {
     val converted = convert(checked)
     converted
   }
-  
-  def drawTree(el:El){
-    
+
+  def drawTree(el: El) {
+
     val map = Map[Element, Int]()
     val buf = new StringBuilder("graph foo{");
-    
+
     def buildLinks(ex: El) {
       ex match {
         case v @ Val(s) =>
@@ -94,7 +94,7 @@ object GraphTool {
 
       }
     }
-    
+
     buildLinks(el)
     buf ++= "}"
 
@@ -109,110 +109,111 @@ object GraphTool {
 
   }
 
-  def buildGraphWizFile(e: Expr){
+  def buildGraphWizFile(e: Expr) {
     val converted = getBinaryTree(e)
     drawTree(converted)
   }
-  
-  def getLeafs(el:El):List[Oper]={
-    el match{
-      case v:Val => Nil
-      case p:Progress => Nil
-      case o@Oper(c ,l:Val, r:Val) => o::Nil 
-      case Oper(_, l,r) => getLeafs(l):::getLeafs(r)
+
+  def getLeafs(el: El): List[Oper] = {
+    el match {
+      case v: Val => Nil
+      case p: Progress => Nil
+      case o @ Oper(c, l: Val, r: Val) => o :: Nil
+      case Oper(_, l, r) => getLeafs(l) ::: getLeafs(r)
     }
   }
-  
-  case class Progress(op:Oper) extends El(rnd.nextInt(1000000))
-  
-  
-  def removeLeaf(el:El, leaf: Oper, p: Progress)={
-    def rm(elem: El):El={
-      elem match{
-        case o:Oper if o.id == leaf.id => p
+
+  case class Progress(op: Oper) extends El(rnd.nextInt(1000000))
+
+  def removeLeaf(el: El, leaf: Oper, p: Progress) = {
+    def rm(elem: El): El = {
+      elem match {
+        case o: Oper if o.id == leaf.id => p
         case Oper(c, l, r) =>
-          val ll = l match{
-            case op:Oper => rm(op)
+          val ll = l match {
+            case op: Oper => rm(op)
             case any => any
           }
-          val rr = r match{
-            case op:Oper => rm(op)
+          val rr = r match {
+            case op: Oper => rm(op)
             case any => any
           }
           Oper(c, ll, rr)
         case x => x
       }
     }
-    
+
     rm(el)
   }
-  
-  def replaceProgress(el:El, leaf:Oper)={
-    def replace(elem: El):El={
-      elem match{
-        case Progress(o) if o==leaf => Val("calculated")
+
+  def replaceProgress(el: El, leaf: Oper) = {
+    def replace(elem: El): El = {
+      elem match {
+        case Progress(o) if o == leaf => Val("calculated")
         case Oper(c, l, r) =>
           Oper(c, replace(l), replace(r))
         case x => x
       }
     }
-    
+
     replace(el)
   }
-  
-  def buildProc(el:El, N:Int){
-    val layers :Array[MutableList[Option[Oper]]] = Array.fill(N)(new MutableList)
-    val current :Array[Option[Oper]] = Array.fill(N)(None)
-    
+
+  def buildProc(el: El, N: Int, print: Boolean = false) = {
+    val layers: Array[MutableList[Option[Oper]]] = Array.fill(N)(new MutableList)
+    val current: Array[Option[Oper]] = Array.fill(N)(None)
+
     var run = true
     var tree = el
-    
-    
+
     implicit var time = 0
-    while(run){
+    while (run) {
       val leafs = tree.leafs
-      
-      current(N-1) foreach(oper => tree = tree.replace(oper))
-      
-      for(i <- (1 to N-1).reverse){
-        current(i) = current(i-1)
+
+      current(N - 1) foreach (oper => tree = tree.replace(oper))
+
+      for (i <- (1 to N - 1).reverse) {
+        current(i) = current(i - 1)
       }
       current(0) = None
-      
+
       val leaf = leafs.headOption
-      leaf foreach(l => tree = tree.rm(l, Progress(l)))
-      
-      println("current leaf = "+leaf)
+      leaf foreach (l => tree = tree.rm(l, Progress(l)))
+
+      if(print) println("current leaf = " + leaf)
       current(0) = leaf
-      val step = current.map{
+      val step = current.map {
         case Some(o) => o.value
         case None => 0
       }.reduce(scala.math.max)
-      println("step = "+step)
-      
-      for(i <- 0 until N;
-    	  c = current(i);
-          l = layers(i);
-    	  j <- 0 until step){
-        val q = c match{
-          case Some(op) if j<op.value => Some(op)
+      if (print) println("step = " + step)
+
+      for (
+        i <- 0 until N;
+        c = current(i);
+        l = layers(i);
+        j <- 0 until step
+      ) {
+        val q = c match {
+          case Some(op) if j < op.value => Some(op)
           case _ => None
         }
-        
+
         l += q
       }
-      
-      time+=step
-      if(time >= 100 || tree.isInstanceOf[Val]) run = false
+
+      time += step
+      if (tree.isInstanceOf[Val]) run = false
     }
-    
-    layers.foreach{list =>
-      println(list.map{
-        case None => "[ ]"
-        case Some(op) => "[%c]" format op.c
-      })
+    if (print) {
+      layers.foreach { list =>
+        println(list.map {
+          case None => "[ ]"
+          case Some(op) => "[%c]" format op.c
+        })
+      }
     }
-    
+
   }
-   
+
 }
